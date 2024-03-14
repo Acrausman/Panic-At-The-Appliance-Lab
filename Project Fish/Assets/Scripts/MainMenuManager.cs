4969493d725e6ee6074163ab9c87471939e96158
Connor-Yi <connordyi@gmail.com>
2024-03-13 20:47:32 -0400

    Main menu and victory scene
    Main menus scene and temp victory scene

---
+++ Project Fish/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    //start game
+    public void StartGame() => SceneManager.LoadScene("Level 1");
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
